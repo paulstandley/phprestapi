cd43edf6c1794f62bf1350b5d76f5856a03d307d
Paul Standley <paulstandley1972@gmail.com>
2019-09-27 13:56:45 +0100

    create post

--- models/Post.php
+++ models/Post.php
@@ -55,7 +55,7 @@
         LEFT JOIN
           categories c ON p.category_id = c.id
         WHERE
-          p.ip = ?
+          p.id = ?
         LIMIT 0,1
         ';
 
@@ -74,6 +74,43 @@
       $this->author = $row['author'];
       $this->category_id = $row['category_id'];
       $this->category_name = $row['category_name'];
+    }
+
+    // Create post
+    public function create() {
+      // Create query named paramiters
+      $query = 'INSERT INTO '.$this->table.'
+        SET
+          title = :title,
+          body = :body,
+          aurtor = :aurthor,
+          category_id = :caterory_id
+      ';
+
+      // Prepare Statment
+      $stmt = $this->conn->prepare($query);
+
+      // Clean data
+      $this->title = htmlspecialchars(strip_tags($this->title));
+      $this->body = htmlspecialchars(strip_tags($this->body));
+      $this->author = htmlspecialchars(strip_tags($this->author));
+      $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+      // Bind data
+      $stmt->bindParam(':title', $this->title);
+      $stmt->bindParam(':body', $this->body);
+      $stmt->bindParam(':author', $this->author);
+      $stmt->bindParam(':category_id', $this->category_id);
+
+      // Execute query
+      if($stmt->execute()) {
+        return true;
+      }
+
+      // Print error
+      printf('Error: %s.\n', $stmt->error);
+
+      return false;
 
     }
 
